52e1e05dd4efcc5b3462010864b8fdf51a649857
wangxin <2909540307@qq.com>
2018-06-11 22:08:05 +0800

    创建Spring boot

---
+++ StudySpringBoot/src/main/java/site/wangxin520/springboot/MyApplication.java
@@ -0,0 +1,34 @@
+package site.wangxin520.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller					//这是spring mvc 的标准访问类的注解
+@SpringBootApplication		//标注了这是一个spring boot的项目，这个是spring boot 的核心注解
+@Configuration				//@Configuration注解表明这是一个spring的配置类
+public class MyApplication {
+
+	/**
+	 * 标准的spring mvc的访问方法
+	 * @return
+	 */
+	@RequestMapping("helloworld")
+	@ResponseBody
+	public String helloWorld() {
+		System.out.println("hello world");
+		return "hello world";
+	}
+
+	/**
+	 * Spring boot的启动入口方法
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+}
